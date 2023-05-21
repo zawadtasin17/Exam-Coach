83e7dede4586d8154f21784e03e4b308b8768124
RGB <120703745+ibrahimamam1@users.noreply.github.com>
2023-05-21 23:54:29 +0600

    Update main_menu.c

--- main_menu.c
+++ main_menu.c
@@ -2,7 +2,10 @@
 #include<conio.h>
 #include "chat.h"
 #include"functions.h"
+#include"scheduler.h"
+#include"problem_solver.h"
 
+#pragma warning(disable : 4996)
 
 int main()
 {
@@ -11,37 +14,35 @@ int main()
 
 int main_menu() {
 	int operation;
-
+	int reminders = checkReminders();
 	printf("\t\t----- EXAM COACH -----\n\n");
+	if (reminders) printf("--You have %d Exams coming up(Check Exam scheduler)\n\n\n" , reminders );
 	printf("1) Practice Problems \n");
 	printf("2) Quick Speed Mode \n");
-	printf("3) Chat with Coach \n");
-	printf("4) Progress Tracking \n");
+	printf("3) Exam Scheduler \n");
+	printf("4) Chat with Coach \n");
 	printf("5) Exit \n\n");
 
-	scanf_s("%d", &operation);
+	scanf("%d", &operation);
 
 	switch (operation)
 	{
 	case 1:
-		//call function for problem solver(From Muntasir's code)
-		system("cls");
-		printf("This will open the problem solver page\n");
+		clear_screen();
+		problem_solver();
 		break;
 	case 2:
-		//call function for quick practice mode (From Tasin's code)
-		system("cls");
-		printf("This will open the quick practice page\n");
+		clear_screen();
+		quick_mode();
 		break;
 	case 3:
-		//call function for interactive chat with coach(Ibrahima's code)
-		system("cls");
-		chat();
+		clear_screen();
+		examManager();
 		break;
 	case 4:
-		//call function for progress tracker
-		system("cls");
-		printf("This will open progress tracker");
+		
+		clear_screen();
+		chat();
 		break;
 	case 5:
 		//exit app
@@ -51,3 +52,4 @@ int main_menu() {
 		printf("invalid input");
 	}
 }
+
