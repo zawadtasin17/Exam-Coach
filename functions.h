6aaae9168a273aee5ea4ea08523309c8b234087c
RGB <120703745+ibrahimamam1@users.noreply.github.com>
2023-05-21 23:54:58 +0600

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,3 +1,12 @@
 #pragma once
+#include<time.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<stdlib.h>
 
+extern char* toLowerCase(char* str);
+extern void setModifiedTime(time_t* str);
 int main_menu();
+void clear_screen();
+int checkReminders();
